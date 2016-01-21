c75768826452c169ac5708e664a3e7dbad7a9936
Jason C Miller <cyzicusgit@gmail.com>
2016-01-21 12:30:04 -0700

    lists all files in a directory

---
+++ functional-programming-java/src/us/cyzic/fpij/compare/ListFiles.java
@@ -0,0 +1,25 @@
+package us.cyzic.fpij.compare;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ListFiles {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("All files and subdirectories:");
+		Files.list(Paths.get("../."))
+			.forEach(System.out::println);
+		System.out.println();
+		
+		
+		// Can filter out files...only list subdirectories
+		System.out.println("Only subdirectories:");
+		Files.list(Paths.get("../."))
+			.filter(Files::isDirectory)
+			.forEach(System.out::println);
+		System.out.println();
+	}
+
+}
