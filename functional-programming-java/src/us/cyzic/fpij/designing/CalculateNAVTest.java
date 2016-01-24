d68f8935d1b10c82d45694cea9cdae6477c77136
Jason C Miller <cyzicusgit@gmail.com>
2016-01-23 18:04:40 -0700

    delegates using lambda expressions

---
+++ functional-programming-java/src/us/cyzic/fpij/designing/CalculateNAVTest.java
@@ -0,0 +1,18 @@
+package us.cyzic.fpij.designing;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+public class CalculateNAVTest {
+
+	@Test
+	public void computeStockWorth() {
+		final CalculateNAV calculateNAV =
+				new CalculateNAV(ticker -> new BigDecimal("6.01"));
+		BigDecimal expected = new BigDecimal("6010.00");
+		assertEquals(0, calculateNAV.computeStockWorth("GOOG", 1000).compareTo(expected), 0.001);
+	}
+}
