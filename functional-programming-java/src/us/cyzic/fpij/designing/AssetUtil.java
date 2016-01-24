ae41a0040880b37702102493b731146a7356d633
Jason C Miller <cyzicusgit@gmail.com>
2016-01-23 17:52:53 -0700

    refactored to separate a key concern

--- functional-programming-java/src/us/cyzic/fpij/designing/AssetUtil.java
+++ functional-programming-java/src/us/cyzic/fpij/designing/AssetUtil.java
@@ -2,8 +2,8 @@ package us.cyzic.fpij.designing;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.function.Predicate;
 
 public class AssetUtil {
 
@@ -13,10 +13,20 @@ public class AssetUtil {
 			new Asset(Asset.AssetType.STOCK, 3000),
 			new Asset(Asset.AssetType.STOCK, 4000));
 	
+	final static Predicate<Asset> stocks = asset -> asset.getType() == Asset.AssetType.STOCK;
+	final static Predicate<Asset> bonds = asset -> asset.getType() == Asset.AssetType.BOND;
+	
 	public static void main(String[] args) {
 		System.out.println("Total value of all assets: " + totalAssetValues(assets));
 		System.out.println("Average value of assets: " + averageAssetValue(assets));
-
+		System.out.println("Total of bonds: " + totalBondValues(assets));
+		System.out.println("Total of stocks: " + totalStockValues(assets));
+		System.out.println("Total of bonds (using inline predicate): " + 
+				totalAssetValues(assets, asset -> asset.getType() == Asset.AssetType.BOND));
+		System.out.println("Total of stocks (using inline predicate): " +
+				totalAssetValues(assets, asset -> asset.getType() == Asset.AssetType.STOCK));
+		System.out.println("Total of bonds (using stored predicate): " + totalAssetValues(assets, bonds));
+		System.out.println("Total of stocks (using stored predicate): " + totalAssetValues(assets, stocks));
 	}
 	
 	public static int totalAssetValues(final List<Asset> assets) {
@@ -33,4 +43,26 @@ public class AssetUtil {
 		return averageValue.isPresent() ? averageValue.getAsDouble() : 0;
 	}
 
+	public static int totalBondValues(final List<Asset> assets) {
+		return assets.stream()
+				.mapToInt(asset -> asset.getType() == Asset.AssetType.BOND 
+					? asset.getValue() : 0)
+				.sum();
+	}
+	
+	public static int totalStockValues(final List<Asset> assets) {
+		return assets.stream()
+				.mapToInt(asset -> asset.getType() == Asset.AssetType.STOCK 
+					? asset.getValue() : 0)
+				.sum();
+	}
+	
+	public static int totalAssetValues(final List<Asset> assets, 
+			final Predicate<Asset> assetSelector) {
+		return assets.stream()
+				.filter(assetSelector)
+				.mapToInt(Asset::getValue)
+				.sum();
+	}
+	
 }
