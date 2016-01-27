d7a7fee13a2e40663f9cf209f6e5d8558341eb7f
Jason C Miller <cyzicusgit@gmail.com>
2016-01-27 12:48:12 -0700

    leverages the laziness of streams

---
+++ functional-programming-java/src/lazy/LazyStreams.java
@@ -0,0 +1,33 @@
+package lazy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LazyStreams {
+
+	private static int length(final String name) {
+		System.out.println("getting length for " + name);
+		return name.length();
+	}
+	
+	private static String toUpper(final String name) {
+		System.out.println("converting to uppercase: " + name);
+		return name.toUpperCase();
+	}
+	
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("Brad", "Kate", "Kim", "Jack", "Joe", "Mike",
+					"Susan", "George", "Robert", "Julia", "Parker", "Benson");
+		
+		final String firstNameWith3Letters = 
+				names.stream()
+					.filter(name -> length(name) == 3)
+					.map(name -> toUpper(name))
+					.findFirst()
+					.get();
+		
+		System.out.println(firstNameWith3Letters);
+
+	}
+
+}
