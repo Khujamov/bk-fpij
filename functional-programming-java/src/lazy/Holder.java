2ff6e680173dfad835dab1ffe0c452182ba90ee0
Jason C Miller <cyzicusgit@gmail.com>
2016-01-27 12:26:16 -0700

    delays initialization

---
+++ functional-programming-java/src/lazy/Holder.java
@@ -0,0 +1,37 @@
+package lazy;
+
+import java.util.function.Supplier;
+
+public class Holder {
+
+	private Supplier<Heavy> heavy = () -> createAndCacheHeavy();
+
+	public static void main(String[] args) {
+		final Holder holder = new Holder();
+		System.out.println("deferring heavy creation...");
+		System.out.println(holder.getHeavy());
+		System.out.println(holder.getHeavy());
+	}
+
+	private synchronized Heavy createAndCacheHeavy() {
+		class HeavyFactory implements Supplier<Heavy> {
+			private final Heavy heavyInstance = new Heavy();
+
+			@Override
+			public Heavy get() {
+				return heavyInstance;
+			}
+		}
+
+		if (!HeavyFactory.class.isInstance(heavy)) {
+			heavy = new HeavyFactory();
+		}
+
+		return heavy.get();
+	}
+	
+	public Heavy getHeavy() {
+		return heavy.get();
+	}
+
+}
