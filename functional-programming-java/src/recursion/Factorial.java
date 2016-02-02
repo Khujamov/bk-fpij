378c877c07d6374ed46941770ab0db903e4d3db4
Jason C Miller <cyzicusgit@gmail.com>
2016-02-02 12:28:14 -0700

    uses tail-recursive function

--- functional-programming-java/src/recursion/Factorial.java
+++ functional-programming-java/src/recursion/Factorial.java
@@ -1,8 +1,5 @@
 package recursion;
 
-//import static us.cyzic.fpij.TailCalls.done;
-//import static us.cyzic.fpij.TailCalls.call;
-
 public class Factorial {
 
 	public static int factorialRec(final int number) {
@@ -12,7 +9,17 @@ public class Factorial {
 			return number * factorialRec(number - 1);
 	}
 	
+	public static TailCall<Integer> factorialTailRec(final int factorial, final int number) {
+		if(number == 1) {
+			return TailCalls.done(factorial);
+		}
+		else {
+			return TailCalls.call(() -> factorialTailRec(factorial * number, number - 1));
+		}
+	}
+	
 	public static void main(String[] args) {
+		
 		
 		// Unoptimized...will throw stack overflow with large number
 		System.out.println("Unoptimized recursion of 5: " + factorialRec(5));
@@ -22,7 +29,10 @@ public class Factorial {
 		} catch (StackOverflowError ex) {
 			System.out.println(ex);
 		}
-
+		
+		System.out.println("Optimized recursion of 5: " + factorialTailRec(1, 5).invoke());
+		System.out.println("Optimized recursion of 20: " + factorialTailRec(1, 20).invoke());
+		
 	}
 
 }
